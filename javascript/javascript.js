396ec841be03c62e41d36ad691b11877990b94fa
Jordan Nelson <jnelsontn@gmail.com>
2017-03-07 21:22:40 -0600

    minimum.VP

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,44 +1,44 @@
 'use strict';
 
+// used to generate HP and Attack Damage
 function randomGenerator (min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let robotList = [];
-
 let Robot = function () {
 	this.usage = 'Military';
 };
+	
+	let robotList = [];
 
 	// Robot 'type' - UAV
 	let UAV = function () {
-		// Robot.call(this);
 		this.geography = 'Air';
 	};
 	UAV.prototype = new Robot();
 
 		// Start UAV Models
-		let UAV_Model1 = function (name) {
+		let UAV_Model1 = function () {
 			this.id = 'UAV_Model1';
-			this.name = name;
 			this.manufacturer = 'Northrop Grumman';
 			this.model = 'MQ8 Fire Scout';
-			this.attack = randomGenerator(20, 35);
-			this.health = randomGenerator(120, 140);
+			this.attack = {'min': 20, 'max': 35};
+			this.health = {'min': 120, 'max': 140};
 		};
 		UAV_Model1.prototype = new UAV();
-		robotList.push(new UAV_Model1() );
+		/* 	is there a better way to add this to a dropdown (dynamically) 
+			then create a new instance? */
+		robotList.push( new UAV_Model1() );
 
-		let UAV_Model2 = function (name) {
+		let UAV_Model2 = function () {
 			this.id = 'UAV_Model2';
-			this.name = name;
 			this.manufacturer = 'Northrop Grumman';
 			this.model = 'RQ-4 Global Hawk';
-			this.attack = randomGenerator(15, 40);
-			this.health = randomGenerator(110, 130);
+			this.attack = {'min': 15, 'max': 40};
+			this.health = {'min': 110, 'max': 130};
 		};
 		UAV_Model2.prototype = new UAV();
-		robotList.push(new UAV_Model2() );
+		robotList.push( new UAV_Model2() );
 
 	// Robot 'type' - UGV
 	let UGV = function () {
@@ -47,27 +47,25 @@ let Robot = function () {
 	UGV.prototype = new Robot();
 
 		// Start UGV Models
-		let UGV_Model1 = function (name) {
+		let UGV_Model1 = function () {
 			this.id = 'UGV_Model1';
-			this.name = name;
 			this.manufacturer = 'Foster-Miller';
 			this.model = 'TALON';
-			this.attack = randomGenerator(10, 20);
-			this.health = randomGenerator(90, 95);
+			this.attack = {'min': 10, 'max': 20};
+			this.health = {'min': 90, 'max': 95};
 		};
 		UGV_Model1.prototype = new UGV();
-		robotList.push(new UGV_Model1() );
+		robotList.push( new UGV_Model1() );
 
-		let UGV_Model2 = function (name) {
+		let UGV_Model2 = function () {
 			this.id = 'UGV_Model2';
-			this.name = name;
 			this.manufacturer = 'Howe & Howe';
 			this.model = 'RipSaw';
-			this.attack = randomGenerator(15, 25);
-			this.health = randomGenerator(100, 120);
+			this.attack = {'min': 15, 'max': 25};
+			this.health = {'min': 100, 'max': 120};
 		};
 		UGV_Model2.prototype = new UGV();
-		robotList.push(new UGV_Model2() );
+		robotList.push( new UGV_Model2() );
 
 	// Robot 'type' - UUV
 	let UUV = function () {
@@ -76,73 +74,69 @@ let Robot = function () {
 	UUV.prototype = new Robot();
 
 		// Start UUV 'Models'
-		let UUV_Model1 = function (name) {
+		let UUV_Model1 = function () {
 			this.id = 'UUV_Model1';
-			this.name = name;
 			this.manufacturer = 'Lockheed';
 			this.model = 'Marlin MK2';
-			this.attack = randomGenerator(40, 60);
-			this.health = randomGenerator(80, 90);
+			this.attack = {'min': 40, 'max': 60};
+			this.health = {'min': 80, 'max': 90};
 		};
 		UUV_Model1.prototype = new UUV();
-		robotList.push(new UUV_Model1() );
+		robotList.push( new UUV_Model1() );
 
-		let UUV_Model2 = function (name) {
+		let UUV_Model2 = function () {
 			this.id = 'UUV_Model2';
-			this.name = name;
 			this.manufacturer = 'Boeing';
 			this.model = 'Voyager';
-			this.attack = randomGenerator(35, 50);
-			this.health = randomGenerator(70, 90);
+			this.attack = {'min': 35, 'max': 50};
+			this.health = {'min': 70, 'max': 90};
 		};
 		UUV_Model2.prototype = new UUV();
-		robotList.push(new UUV_Model2() );
+		robotList.push( new UUV_Model2() );
 
-
-
-// Populate drop-down with Robot Models
+/* 	Populate the Dropdown with our models from above
+	Perhaps there is a better way to do this than using 'new' ? */
 for ( var i = 0; i < robotList.length; i++ ) {
 
-	let dropdown = '';
-	dropdown += `<option id="${robotList[i].id}">${robotList[i].model}</option>`;
+	let dropdown = ''; // rather declare it here than globally...and i don't need a func. for this :)
+	dropdown += `<option id="${robotList[i].id}">${robotList[i].manufacturer} ${robotList[i].model}</option>`;
 
 	$('#robot1_dropdown').append( dropdown );
 	$('#robot2_dropdown').append( dropdown );
-
 }
 
-
-// Select From Dropdown
+// Only solution I've found is to use eval() otherwise the funct.exp won't be called
 $('#robot_button').click( () => {
 
-	let para1 = $('#robot1_dropdown option:selected').attr('id');
-	let para2 = $('#robot2_dropdown option:selected').attr('id');
+	let para1 = eval ( $('#robot1_dropdown option:selected').attr('id') );
+	let para2 = eval ( $('#robot2_dropdown option:selected').attr('id') );
 
 	let name1 = $('#robot1_name').val();
-	let name2 = $('#robot2_name').val(); 
+	let name2 = $('#robot2_name').val();
 
-	/* if ( para1 === 'None' || para2 === 'None' || name1 === '' || name2 === '') {
-		alert('Complete All Fields');
+	if ( (para1 === 'None' || para2 === 'None' || name1 === '' || name2 === '') || (name1 === name2) ) {
+		alert('Complete All Fields and Choose Seperate Names');
 		return false;
-	} */
+	}
 
-	console.log(para1, para2);
-	battleBots( para1, para2, name1, name2 );
-	// battleBots(UGV_Model2, UGV_Model1);
+	battleBots(new para1(), new para2(), name1, name2 );
 
 });
 
-function battleBots ( Robot1, Robot2, setName1, setName2) {
+function battleBots (battle_bot1, battle_bot2, setName1, setName2) {
+	$('.winlose').empty();
+
+	battle_bot1.name = setName1;
+	battle_bot2.name = setName2;
+
+	battle_bot1.health = randomGenerator(battle_bot1.health.min, battle_bot1.health.max);
+	battle_bot1.attack = randomGenerator(battle_bot1.attack.min, battle_bot1.attack.max);
+
+	battle_bot2.health = randomGenerator(battle_bot2.health.min, battle_bot2.health.max);
+	battle_bot2.attack = randomGenerator(battle_bot2.attack.min, battle_bot2.attack.max);
 	
-	//console.log(Robot1);
-
-	let battle_bot1 = new Robot1();
-		battle_bot1.name = setName1;
-		console.log(battle_bot1);
-
-	let battle_bot2 = new Robot2();
-		battle_bot2.name = setName2;
-		console.log(battle_bot2);
+	console.log('Player 1: ', battle_bot1);
+	console.log('Player 2: ', battle_bot2);
 
 	if (battle_bot1.health > 0 || battle_bot2.health > 0) {
 
@@ -150,32 +144,36 @@ function battleBots ( Robot1, Robot2, setName1, setName2) {
 
 		while (true) {
 
-			console.log('Round Number', numRound);
-			console.log('------------------------');
+			console.log('Round Number: ', numRound);
 
 			battle_bot1.health = battle_bot1.health - battle_bot2.attack;
-			console.log(battle_bot2.name + ' hit for ' + battle_bot2.attack + '. ' + battle_bot1.name + ' health is now ' + battle_bot1.health);
+			console.log(battle_bot2.name + ' attacks ' + battle_bot1.name + ' for ' + battle_bot2.attack + 'hp. ' + battle_bot1.name + `'s` + ' health is now ' + battle_bot1.health);
 
 			if (battle_bot1.health <= 0) {
 				battle_bot1.health = 0;
-				console.log(battle_bot1.name + ' has lost' );
+				console.log(battle_bot1.name + ' has lost. ' + battle_bot2.name + ' wins.' );
+
+				let toWebpage = `${battle_bot1.name} has LOST (LOSER!!!)` + '<br /><br />' + '<b>' + `${battle_bot2.name} has WON (WINNER!!!!)` + '</b>';
+				$('.winlose').append(toWebpage);
+
 				break;
 			}
 
 			battle_bot2.health = battle_bot2.health - battle_bot1.attack;
-			console.log(battle_bot1.name + ' hit for ' + battle_bot1.attack + '. ' + battle_bot2.name + ' health is now ' + battle_bot2.health);
+			console.log(battle_bot1.name + ' attacks ' + battle_bot2.name + ' for ' + battle_bot1.attack + 'hp. ' + battle_bot2.name + `'s` + ' health is now ' + battle_bot2.health);
 
 			if (battle_bot2.health <= 0) {
 				battle_bot2.health = 0;
-				console.log(battle_bot2.name + ' has lost' );
+				console.log(battle_bot2.name + ' lost. ' + battle_bot1.name + ' wins.' );
+
+				let toWebpage = `${battle_bot2.name} has LOST (LOSER!!!)` + '<br /><br />' + '<b>' + `${battle_bot1.name} has WON (WINNER!!!!)` + '</b>';
+				$('.winlose').append(toWebpage);
+
 				break;
 			}
 
-			// battle_bot1.attack = randomGenerator(20, 35);
-			// battle_bot2.attack = randomGenerator(40, 60);
-
 			numRound++;
-		} // end while loop
+		}
 	}
 
 
